89598c57c1e0a70a25148735250d8e5e82ac544f
ramanans <s.venkatramanan@gmail.com>
2024-03-22 15:58:50 +0530

    Create Request.java

---
+++ Request.java
@@ -0,0 +1,14 @@
+package ai.djl.examples.doodle;
+
+public class Request {
+
+    private String imageData;
+
+    public String getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(String imageData) {
+        this.imageData = imageData;
+    }
+}
